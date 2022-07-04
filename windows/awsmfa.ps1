a1aee84ee06d8bd2d693cf4f434df5e7f05faf87
Joaquim Gomez <49433786+JoaquimXG@users.noreply.github.com>
2022-07-04 17:12:11 +0100

    Creating windows script for starting AWS MFA session

---
+++ windows/awsmfa.ps1
@@ -0,0 +1,75 @@
+<#
+.Synopsis
+    Starts an AWS session that requires MFA
+.EXAMPLE
+    Get-AwsSession <token>
+.EXAMPLE
+    Get-AwsMfaSession -MfaToken <token> -AwsProfile <profile>
+   Another example of how to use this cmdlet
+#>
+function Get-AwsMfaSession {
+    [CmdletBinding()]
+    Param
+    (
+        # MFA Token
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        $MfaToken,
+
+        # MFA Token
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 1)]
+        $AwsProfile
+    )
+
+    Begin {
+        if ($Env:AWS_PROFILE -eq $null) {
+            if ($AwsProfile -eq $null) {
+                Write-Host "Enter the name of the AWS profile to use: " -NoNewLine
+                $AwsProfile = Read-Host
+            }
+        } else {
+            if ($AwsProfile -eq $null) {
+                $AwsProfile = $Env:AWS_PROFILE
+            } else {
+                Write-Host "Overriding AWS_PROFILE environment variable with: $AwsProfile"
+            }
+        }
+        
+        Write-Host "Using AWS_PROFILE: $AwsProfile"
+        
+        if ($MfaToken -eq $null) {
+            Write-Host "Enter the MFA token: " -NoNewLine
+            $MfaToken = Read-Host
+        }
+    }
+    Process {
+        $mfa_serial = (aws configure get mfa_serial --profile $AwsProfile)
+        write-host $mfa_serial
+        
+        try {
+            $creds = (aws sts get-session-token --serial-number $mfa_serial --token-code $MfaToken --profile $AwsProfile |
+            ConvertFrom-Json).Credentials
+
+            if ($creds -eq $null) {
+                Write-Host "Failed to start session" -ForegroundColor Red
+                return
+            }
+
+            $Env:AWS_PROFILE = $AwsProfile
+            $Env:AWS_ACCESS_KEY_ID = $creds.AccessKeyId
+            $Env:AWS_SECRET_ACCESS_KEY = $creds.SecretAccessKey
+            $Env:AWS_SESSION_TOKEN = $creds.SessionToken
+
+            Write-Host "Session started for $AwsProfile" -ForegroundColor Green
+        }
+        catch {
+            Write-Host "Failed to start session" -ForegroundColor Red
+            return
+        }
+    }
+    End {
+    }
+}
